d115eb157e17e9cb8668389c9205a8119c69377d
hutingting119 <1912257350@qq.com>
2016-08-05 09:50:32 +0800

    can turn

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 const App=React.createClass({
+        getInitialState:function () {
+                return {
+                        isEditor:true
+                }
+        },
+        toggle:function () {
+          this.setState({
+                  isEditor:!this.state.isEditor
+          })
+        },
         render:function(){
-        return <div>App</div>
+        return <div>
+                <button onClick={this.toggle}>{this.state.isEditor?"Previewer":"Editor"}</button>
+                <div className={this.state.isEditor?"":"hidden"}>
+                        <Editor />
+                </div>
+                <div className={this.state.isEditor?"hidden":""}>
+                        <Previewer />
+                </div>
+        </div>
 
+        }
+})
+
+const Previewer=React.createClass({
+        render:function(){
+        return <div>Previewer</div>
+        
+        }
+})
+
+const Editor=React.createClass({
+        render:function(){
+        return <div>Editor</div>
+        
         }
 })
 ReactDOM.render(<App/>,document.getElementById('content'));
